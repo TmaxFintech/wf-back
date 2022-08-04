771d5e1af08ed5b976ab844ee46169fe0eca7e9b
coshin <mincolong@naver.com>
2022-08-04 23:19:42 +0900

    |Feat| Change SecurityConfig |

--- src/main/java/tmaxfintech/wf/config/SecurityConfig.java
+++ src/main/java/tmaxfintech/wf/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -12,8 +14,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests()
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .addFilterBefore(new CustomFilter("/users/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
                 .anyRequest().permitAll();
     }
-
 }

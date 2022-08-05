3a9cede8c2c6b80746215d259a4969c339822342
coshin71 <101234354+coshin71@users.noreply.github.com>
2022-08-05 13:24:00 +0900

    Kys/f/new/#6 securitylogin (#7)
    * |Feat| Change SecurityConfig |
    
    * |Feat| Add spring security login API |

---
+++ src/main/java/tmaxfintech/wf/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,24 @@
+package tmaxfintech.wf.config.auth;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import tmaxfintech.wf.domain.user.entity.User;
+import tmaxfintech.wf.domain.user.repository.UserRepository;
+
+@Service
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public PrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByUsername(username);
+        return new PrincipalDetails(userEntity);
+    }
+}

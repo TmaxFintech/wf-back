608563d1b889a2d14fd3dbf48ff575e0b48b98db
kijun2423db <kijun2423db@naver.com>
2022-08-11 16:14:35 +0900

    |Chore| Change cors setting |

--- src/main/java/tmaxfintech/wf/config/CorsConfig.java
+++ src/main/java/tmaxfintech/wf/config/CorsConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import tmaxfintech.wf.config.jwt.JwtProperty;
 
 @Configuration
 public class CorsConfig {
@@ -14,9 +15,12 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("http://localhost:3000");
         config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addExposedHeader(JwtProperty.HEADER_STRING);
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }

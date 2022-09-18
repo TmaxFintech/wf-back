c496cc387538629a164bfd0a2762a4c501c08872
coshin <mincolong@naver.com>
2022-09-18 20:30:02 +0900

    |Feat| Add Transactions API |
    사용자가 거래내역을 확인할 수 있도록 거래내역을 반환해주는 API를 구현했습니다.
    거래내역은 거래 유형(매수/매도)을 선택하여 반환하도록 하였습니다.
    선택하지 않으면 거래 유형(매수/매도)을 선택하지 않으면 매수/매도 내역을 모두 반환합니다.

--- src/main/java/tmaxfintech/wf/domain/transactions/repository/TransactionsRepository.java
+++ src/main/java/tmaxfintech/wf/domain/transactions/repository/TransactionsRepository.java
@@ -1,7 +1,16 @@
 package tmaxfintech.wf.domain.transactions.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tmaxfintech.wf.domain.coinAccount.entity.CoinAccount;
 import tmaxfintech.wf.domain.transactions.entity.Transactions;
 
+@Repository
 public interface TransactionsRepository extends JpaRepository<Transactions, Long> {
+
+    Page<Transactions> findAllByCoinAccount(Pageable pageable, CoinAccount coinAccount);
+
+    Page<Transactions> findAllByCoinAccountAndSide(Pageable pageable, CoinAccount coinAccount, String side);
 }

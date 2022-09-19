032f6c0537d89710ae5b69584069a7cc06a3310a
kijun2423db <kijun2423db@naver.com>
2022-09-19 10:56:28 +0900

    |Feat| Add PredictedPriceRepository |
    |가격 예측 레포지토리(인터페이스) 작성|
    
    |Ref: #21|

---
+++ src/main/java/tmaxfintech/wf/domain/predictedPrice/repository/PredictedPriceRepository.java
@@ -0,0 +1,10 @@
+package tmaxfintech.wf.domain.predictedPrice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tmaxfintech.wf.domain.predictedPrice.entity.PredictedPrice;
+
+import java.util.Optional;
+
+public interface PredictedPriceRepository extends JpaRepository<PredictedPrice, Long> {
+    Optional<PredictedPrice> findBySymbol(String symbol);
+}

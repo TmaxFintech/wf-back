eb4448f50b3f9bc88ba4a67c6111225b367550bf
kijun2423db <kijun2423db@naver.com>
2022-09-19 10:58:45 +0900

    |Feat| Add PredictedPriceEntity |
    |가격 예측 API 엔티티 작성|
    
    |Ref: #21|

---
+++ src/main/java/tmaxfintech/wf/domain/predictedPrice/entity/PredictedPrice.java
@@ -0,0 +1,35 @@
+package tmaxfintech.wf.domain.predictedPrice.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class PredictedPrice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String symbol;
+    private Double predictedPrice;
+
+    protected PredictedPrice(){
+
+    }
+
+    public PredictedPrice(String symbol, Double predictedPrice){
+        this.symbol = symbol;
+        this.predictedPrice = predictedPrice;
+    }
+
+    public String getSymbol(){return symbol;}
+    public Double getPredictedPrice(){return predictedPrice;}
+
+    public void updatePredictedPrice(String symbol, Double predictedPrice){
+        this.symbol = symbol;
+        this.predictedPrice = predictedPrice;
+    }
+
+}

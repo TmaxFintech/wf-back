c1d77f0a3fe8573c23b34957c6f3eb971ae92058
kijun2423db <kijun2423db@naver.com>
2022-09-19 11:00:37 +0900

    |Feat| Add PredictedPriceService |
    |가격 예측 API Service 작성|
    
    |Ref: #21|

---
+++ src/main/java/tmaxfintech/wf/domain/predictedPrice/service/PredictedPriceService.java
@@ -0,0 +1,32 @@
+package tmaxfintech.wf.domain.predictedPrice.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import tmaxfintech.wf.domain.predictedPrice.entity.PredictedPrice;
+import tmaxfintech.wf.domain.predictedPrice.repository.PredictedPriceRepository;
+import tmaxfintech.wf.util.response.DefaultResponse;
+
+@Service
+public class PredictedPriceService {
+
+    private final PredictedPriceRepository predictedPriceRepository;
+
+    @Value("${responseMessage.PREDICTED_PRICE_UPDATE_SUCCESS}")
+    private String PREDICTED_PRICE_UPDATE_SUCCESS;
+
+    public PredictedPriceService(PredictedPriceRepository predictedPriceRepository) {
+        this.predictedPriceRepository = predictedPriceRepository;
+    }
+
+    @Transactional
+    public ResponseEntity<DefaultResponse> updatePredictedPrice(String symbol, Double predictedPrice) {
+
+        PredictedPrice predictedprice = predictedPriceRepository.findBySymbol(symbol).get();
+        predictedprice.updatePredictedPrice(symbol,predictedPrice);
+
+        return new ResponseEntity(DefaultResponse.response(HttpStatus.OK.value(), PREDICTED_PRICE_UPDATE_SUCCESS), HttpStatus.OK);
+    }
+}
